78a50925ef1908f4d293bdd9fa39e95fb392799e
Newton Costa <newtongomez04@gmail.com>
2020-08-22 10:01:03 +0100

    Example 2 completed

--- script/js.js
+++ script/js.js
@@ -3,25 +3,5 @@ var script = document.createElement('script');
 script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAezdKjLeqB-080TI8RXvXViHZ9PV3b3xo&callback=initMap&libraries=&v=weekly"
 script.defer = true;
 
-// Map 1 - With Mark
-/** 
- function initMap() {
-    var bna = {lat: -8.8105164, lng: 13.198614};
-
-    var map = new google.maps.Map(document.getElementById("map"),
-        {zoom: 12, 
-        center: bna
-        });
-    
-    var marker = new google.maps.Marker({position: bna, map: map});   
-}
-*/
 document.head.appendChild(script);
-
-
-
-
-// $(document).ready(function () { 
-//    initMap();
-// });
 
