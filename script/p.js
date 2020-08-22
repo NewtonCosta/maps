7f6d267f5504fdc09f4d2a635a8f2d2b44b9ae1c
Newton Costa <newtongomez04@gmail.com>
2020-08-22 10:28:06 +0100

    Finalized exemplos

--- script/p.js
+++ script/p.js
@@ -1,16 +1,6 @@
 var script = document.createElement('script');
-script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAezdKjLeqB-080TI8RXvXViHZ9PV3b3xo&callback=initMap&libraries=&v=weekly"
+script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAezdKjLeqB-080TI8RXvXViHZ9PV3b3xo&libraries=visualization"
 script.defer = true;
 
-function initMap() {
-    var bna = {lat: -8.8105164, lng: 13.198614};
-
-    var map = new google.maps.Map(document.getElementById("map"),
-        {zoom: 12, 
-        center: bna
-        });
-    
-    var marker = new google.maps.Marker({position: bna, map: map});   
-}
 
 document.head.appendChild(script);
