484eb7e2896698570c44bb8d1f62ec2372215430
Newton Costa <newtongomez04@gmail.com>
2020-09-02 10:59:38 +0100

    Delete p.js

--- script/p.js
+++
@@ -1,6 +0,0 @@
-var script = document.createElement('script');
-script.src = "https://maps.googleapis.com/maps/api/js?key=AIzaSyAezdKjLeqB-080TI8RXvXViHZ9PV3b3xo&libraries=visualization"
-script.defer = true;
-
-
-document.head.appendChild(script);
